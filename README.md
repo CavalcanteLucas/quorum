45488e5d807e834cc77f89665c9b816d8ae4e940
Lucas Cavalcante <lucascpcavalcante@gmail.com>
2023-05-03 13:46:12 -0300

    commit

---
+++ README.md
@@ -0,0 +1,103 @@
+# Quorum
+
+This application is a simple script to query legislative data.
+
+## Running the application
+
+### Locally
+
+1. Clone the repository
+2. Create a virtualenv with
+```
+virtualenv -p python3 venv
+```
+3. Activate the virtualenv with
+```
+source venv/bin/activate
+```
+4. Install the dependencies with
+```
+pip install -r requirements.txt
+```
+5. Run the application with[^1]
+```
+python quorum.py
+```
+or
+```
+python quorum.py --debug
+```
+
+[^1]: For more information run `python quorum.py --help`
+
+### With Docker
+
+Clone the repository, and run:
+```
+docker-compose up --build
+```
+
+
+## Discussion points
+
+> 1. On the solution’s time complexity and the tradeoffs.
+
+a. The complete solution has two main parts, one for each deliverable. The complexity of the first part depends on the method `get_legislator_vote_count`. The complexity of the second part depends on the method `get_bill_vote_counts`.
+
+The method `get_legislator_vote_count` is composed of two main parts:
+
+i. Grouping and aggregating data, which requires sorting the data, thus taking **O(N log N)** time, where **N** is the number of rows in `vote_results_df`.
+
+ii. Merging dataframes, which also requires sorting the data, thus taking **O(M log M)** time, where **M** is the number of rows in `legislators_df`.
+
+All other operations require linear time, thus having a complexity **O(1)**.
+
+Therefore, the overall time complexity of `get_legislator_vote_count` is **O(N log N + M log M)**.
+
+The method `get_bill_vote_counts` is also composed of two main parts:
+
+i. Grouping and aggregating data, which requires sorting the data, thus taking **O(N log N)** time, where **N** is the number of rows in `vote_results_df`.
+
+ii. Joining tables, which can vary depending on the type of the join, the size of the tables, and the number of common columns. Assuming standard inner-joins, the time complexity of each join operation is **O(M log M)**, where **M** is the number of rows in the largest table.
+
+All other operations require linear time, thus having a complexity **O(1)**.
+
+Therefore, the overall time complexity of `get_bill_vote_counts` is **O(N log N + 3M log M)**.
+
+
+b. Some tradeoffs of the given solution include:
+- **Efficiency vs. readability**: the solution is concise and easy to read. However, in can be memory-intensive and slow for large datasets.
+- **Accuracy vs speed**: the solution performs several left-join operations. This can be faster than other types of joins, but it can also lead to inaccurate results if the data is not properly cleaned beforehand.
+- **Flexibility vs. complexity**: the solution is specialized for the specific data format and task at hand. If the data format changes or the requirements change, the solution will need to be updated.
+
+
+> 2. On changing the to account for future columns that might be requested, such as “Bill Voted On Date” or “Co-Sponsors”:
+
+Now that we have a new column in `provided_data/bills.csv`, if such a new information is not required in the output, no changes would be required in the main program. Otherwise, the method `get_bill_vote_counts` would need to be updated to include the new column in the returned dataframe:
+
+```
+return bill_vote_counts[
+    [
+        "id",
+        "title",
+        "supporter_count",
+        "opposer_count",
+        "primary_sponsor",
+        "bill_voted_on_date",  # Add the new column
+    ]
+]
+```
+
+> 3. On chaning the solution solution in case of a list of legislators or bills instead of a CSV:
+
+The solution I would choose is to create a function for converting the list of legislators or bills into a pandas DataFrame. This function could accept a list of dictionaries, where each dictionary represents a legislator or bill with its respective properties, and return a DataFrame with the same structure as the one currently loaded from the CSV files. That way the rest of the program would not need to be changed.
+
+
+> 4. Time spent:
+
+This assignment took me between 3 to 5 hours to complete, plus 1 hour to answer the questions above.
+
+
+### Author
+
+**Quorum** is developed by **Lucas C Cavalcante**. If you have any questions or feedback about the app, please feel free to contact me at lucascpcavalcante@gmail.com.
---
+++ quorum.py
@@ -0,0 +1,206 @@
+import os
+from pathlib import Path
+import pandas as pd
+from enum import Enum
+import logging
+import argparse
+
+
+PROVIDED_DATA_PATH = Path("provided_data")
+DELIVERABLE_DATA_PATH = Path("deliverable_data")
+
+class VoteType(Enum):
+    """
+    Enum for vote types.
+    Assuming 'vote_type' 1 is support and 'vote_type' 2 is oppose.
+    """
+
+    SUPPORT = 1
+    OPPOSE = 2
+
+
+def load_csv_file(
+    filename: str,
+    data_path: Path = PROVIDED_DATA_PATH,
+) -> pd.DataFrame:
+    """Load data from csv file."""
+    csv_path = os.path.join(data_path, filename)
+    return pd.read_csv(csv_path)
+
+
+def save_csv_file(
+    result_df: pd.DataFrame,
+    filename: str,
+    data_path: Path = DELIVERABLE_DATA_PATH,
+) -> None:
+    """Save data to csv file."""
+    target_path = os.path.join(data_path, filename)
+    result_df.to_csv(target_path, index=False)
+    logging.info(f" [x] CSV saved in {target_path}")
+
+
+
+def get_legislator_vote_count(
+    legislators_df: pd.DataFrame,
+    vote_results_df: pd.DataFrame,
+) -> pd.DataFrame:
+    """
+    This method returns the first deliverable, which asks for the following:
+        - For every legislator, how many bills did they support?
+        - For every legislator, how many bills did they oppose?
+
+    The answer is a dataframe with the columns "num_supported_bills" and
+    "num_opposed_bills" added to the legislators dataframe. These columns contain
+    the number of bills supported and opposed, respectively, by each legislator.
+    """
+
+    vote_counts = (
+        vote_results_df.groupby(
+            [
+                "legislator_id",
+                "vote_type",
+            ]
+        )
+        .size()
+        .unstack(fill_value=0)
+        .reset_index()
+    ).rename(
+        columns={
+            VoteType.SUPPORT.value: "num_supported_bills",
+            VoteType.OPPOSE.value: "num_opposed_bills",
+        }
+    )
+
+    legislator_vote_count = (
+        legislators_df.merge(
+            vote_counts,
+            left_on="id",
+            right_on="legislator_id",
+            how="left",
+        )
+        .drop(columns=["legislator_id"])
+        .fillna(0)
+    )
+
+    return legislator_vote_count[
+        [
+            "id",
+            "name",
+            "num_supported_bills",
+            "num_opposed_bills",
+        ]
+    ]
+
+
+def get_bill_vote_counts(
+    bills_df: pd.DataFrame,
+    vote_results_df: pd.DataFrame,
+    votes_df: pd.DataFrame,
+    legislators_df: pd.DataFrame,
+) -> pd.DataFrame:
+    """
+    This method returns the second deliverable, which asks for the following:
+        - For every bill, how many legislators supported it?
+        - For every bill, how many legislators opposed it?
+        - Who was the sponsor of the bill?
+
+    The answer is a dataframe with the columns "supporter_count",
+    "opposer_count", and "legislator_name" added to the bills dataframe.
+    These columns contain the number of legislators who supported and opposed each
+    bill, respectively, and the name of the legislator who sponsored the bill.
+    """
+
+    vote_counts = (
+        vote_results_df.groupby(
+            [
+                "vote_id",
+                "vote_type",
+            ]
+        )
+        .size()
+        .unstack(fill_value=0)
+        .reset_index()
+    ).rename(
+        columns={
+            VoteType.SUPPORT.value: "supporter_count",
+            VoteType.OPPOSE.value: "opposer_count",
+        }
+    )
+
+    bill_vote_counts = (
+        votes_df.merge(
+            vote_counts,
+            left_on="id",
+            right_on="vote_id",
+        )
+        .merge(
+            bills_df,
+            left_on="bill_id",
+            right_on="id",
+        )
+        .merge(
+            legislators_df,
+            left_on="sponsor_id",
+            right_on="id",
+            how="left",
+        )
+        .drop(columns=["vote_id", "id_x", "id_y", "id"])
+        .fillna("Unavailable")
+        .drop(columns=["sponsor_id"])
+    ).rename(
+        columns={
+            "name": "primary_sponsor",
+            "bill_id": "id",
+        }
+    )
+
+    # return bill_vote_counts
+    return bill_vote_counts[
+        [
+            "id",
+            "title",
+            "supporter_count",
+            "opposer_count",
+            "primary_sponsor",
+        ]
+    ]
+
+
+def main() -> None:
+    # Setup file names
+    # Provided data
+    BILLS_FILE = "bills.csv"
+    LEGISLATORS_FILE = "legislators.csv"
+    VOTE_RESULTS_FILE = "vote_results.csv"
+    VOTES_FILE = "votes.csv"
+    # Deliverable data
+    LEGISLATOR_VOTE_COUNT_FILE = "legislator_vote_count.csv"
+    BILLS_VOTE_COUNT_FILE = "bills.csv"
+
+    # Load data
+    bills_df = load_csv_file(BILLS_FILE)
+    legislators_df = load_csv_file(LEGISLATORS_FILE)
+    vote_results_df = load_csv_file(VOTE_RESULTS_FILE)
+    votes_df = load_csv_file(VOTES_FILE)
+
+    # First deliverable
+    legislator_vote_count_df = get_legislator_vote_count(legislators_df, vote_results_df)
+    save_csv_file(legislator_vote_count_df, LEGISLATOR_VOTE_COUNT_FILE)
+    logging.debug(f"\n{legislator_vote_count_df.to_string()}\n")
+
+    # Second deliverable
+    bill_vote_counts_df = get_bill_vote_counts(bills_df, vote_results_df, votes_df, legislators_df)
+    save_csv_file(bill_vote_counts_df, BILLS_VOTE_COUNT_FILE)
+    logging.debug(f"\n{bill_vote_counts_df.to_string()}\n")
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(description='Quorum Application')
+    parser.add_argument('--debug', action='store_true', help='Debug mode')
+    args = parser.parse_args()
+
+    debug_level = logging.DEBUG if args.debug else logging.INFO
+    logging.basicConfig(level=debug_level)
+
+    main()
